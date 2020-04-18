a4d7e9463990ee8f8e40d2c1b69c1d5f7b888c9e
parkerbull1 <48692769+parkerbull1@users.noreply.github.com>
2020-04-18 13:26:35 -0400

    Add files via upload

---
+++ Image.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013-2015, Rethink Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the Rethink Robotics nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Baxter RSDK Inverse Kinematics Pick and Place Demo
+"""
+import numpy as np
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import Int8
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+    def __init__(self):        
+	self.image_pub = rospy.Publisher("Camera", Image, queue_size=10)
+        self.image_sub = rospy.Subscriber("/cameras/right_hand_camera/image", Image, self.Callback)
+	self.bridge = CvBridge()
+
+    def Callback(self,data):
+        try: 
+	    cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+	    print(e)
+
+        #masking can go here
+
+        cv2.imshow("Camera Feed", cv_image)
+	
+	try:
+	    self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+	except CvBridgeError as e:
+	    print(e)
+
+	cv2.waitKey(3)
+
+	
+
+def main(args):
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+
+    try:
+	rospy.spin()
+    except KeyboardInterrupt:
+	print("Shutting Down")
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+  main(sys.argv)
+
